6481d343fe7c51cda1b02c4177b3bd168b4df86e
박겸영 <yong_drg@naver.com>
2023-05-17 21:39:34 +0900

    [REFACTOR] : react-dom v18 문법으로 리팩토링

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import ReactDOM from 'react-dom';
 import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root'),
 );
